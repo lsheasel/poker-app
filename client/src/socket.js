ba29e0c19637454995ea8133c5e02a9dbcf92ef5
Shease <83541534+lsheasel@users.noreply.github.com>
2025-04-29 11:56:36 +0200

    Server Change

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3002", {
+const socket = io("https://poker-app-backend.onrender.com", {
   transports: ['websocket'],
   reconnection: true,
   reconnectionAttempts: 5,
