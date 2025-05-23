edea54b64be1c7d97311a561acdb3f550b78ee10
Shease <83541534+lsheasel@users.noreply.github.com>
2025-05-23 08:43:31 +0200

    Team Change

--- team/src/main.jsx
+++ team/src/main.jsx
@@ -1,10 +1,10 @@
 // src/main.jsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './router';
-import { AuthContextProvider } from './context/AuthContext.jsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './router'
+import { AuthContextProvider } from './context/AuthContext'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -12,4 +12,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <RouterProvider router={router} />
     </AuthContextProvider>
   </React.StrictMode>
-);
+)
